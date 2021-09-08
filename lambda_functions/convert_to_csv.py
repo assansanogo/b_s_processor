f1a2f574e74f1b5cc2fc1c3056d99ea676151e6a
Assan Sanogo <assansanogo@gmail.com>
2021-09-08 11:03:51 +0200

    cleaner_code with helper functions

--- lambda_functions/convert_to_csv.py
+++ lambda_functions/convert_to_csv.py
@@ -11,6 +11,27 @@ import pandas as pd
 GT_HEADER = ["Trans. Date","Value. Date","Reference","Debits","Credits","Balance","Originating Branch","Remarks"]
 
 
+def simple_df_clean(m_df):
+    '''
+    function which cleans non informative data ("Remarks and Trans.date")
+    ocurring while tabula extraction
+    '''
+    m_df = m_df[m_df['Remarks']!= 'Remarks']
+    m_df = m_df[m_df['Remarks']!= 'Balance as at Last Transaction.']
+    m_df = m_df[m_df["Trans. Date"] != 'Trans. Date']
+    return m_df
+
+def transactions(m_df):
+    '''
+    function which keeps transactions index, the last transaction idx and adds an artificial last transaction
+    '''
+    transactions_df = m_df[~m_df["Trans. Date"].isna().copy()
+    transactions_idx = list(transactions_df.index())
+    max_transactions_idx = max(transactions_idx)
+    transactions_df.loc[max_idx + 1,'Trans. Date'] = '99-Apr-9999'
+    return  transactions_df,transactions_idx,max_transactions_idx
+                           
+                           
 def process_bank_statements(b_statement, out_format ='csv'):
     '''
     Method to transform a list of Bank statements paths into a list of .CSV
@@ -21,7 +42,7 @@ def process_bank_statements(b_statement, out_format ='csv'):
         b_statement = [b_statement]
     n_statements = len(b_statement)
     assert n_statements !=0
-    print(b_statement)
+                           
     #loop over the list of bank statements 
     for idx, bk_st in tqdm(enumerate(b_statement)):
         #input filename
@@ -33,16 +54,30 @@ def process_bank_statements(b_statement, out_format ='csv'):
         out = bk_st.replace(".pdf","_output.csv")
         
         # convert to csv by default
-        df = tabula.read_pdf(bk_st, multiple_tables=True, lattice= True, pages='all')
-        header_shape = df[1].shape[1]
-        df_list = [dataframes for dataframes in df if dataframes.shape[1] !=2]
+        df_list = tabula.read_pdf(bk_st, multiple_tables=True, lattice= True, pages='all')
+        header_shape = df_list[1].shape[1]
+        df_list = [datafram for datafram in df_list if (datafram.shape[1] !=2 and datafram.shape[0]!=0) ]
         for i,d in enumerate(df_list):
             try:
+                # set the header to each sub dataframe
                 d.columns = GT_HEADER
+                # save locally for debug purposes
                 d.to_csv(out.replace(".csv",f"{str(i)}.csv"), sep=';')
             except Exception as e:
+                # if the dataframe is malformed remove it from the list of processable dataframes
                 print(i)
                 df_list.remove(d)
+        
+        # concat each dataframe
+        master_df = pd.concat(df_list)
+                           
+        # clean non informative cells
+        master_df = simple_df_clean(master_df)
+        
+        # store informations about the transactions
+        tr_df, tr_idx, max_tr_idx = transactions(master_df)
+        
+        # json response
         response[str(idx)] = {"name":bk_st, "body":[d.reset_index(drop=True).to_json() for d in df_list]}
         return response
         
