a6033bd874ea3a0a4502605c756ace1b1217b75b
Assan Sanogo <assansanogo@gmail.com>
2021-09-10 21:29:18 +0200

    Update clean_and_classify.py

--- lambda_functions/clean_and_classify.py
+++ lambda_functions/clean_and_classify.py
@@ -48,7 +48,8 @@ def process_descriptions(sentences):
 def clean_bank_statements(file_name, out_format):
     df = pd.read_csv(file_name, sep=';')
     #df["filtered_description"] = df["Remarks_processed"].str.upper()
-    return str(list(df.columns))
+    
+    return str(df.shape[0])
     
 
 
