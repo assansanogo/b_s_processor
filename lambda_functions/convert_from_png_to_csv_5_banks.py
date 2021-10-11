1919d1a52ad01f706ea2b32444e3e7419c97e2d2
Assan Sanogo <assansanogo@gmail.com>
2021-10-11 18:39:16 +0200

    Update convert_from_png_to_csv_5_banks.py

--- lambda_functions/convert_from_png_to_csv_5_banks.py
+++ lambda_functions/convert_from_png_to_csv_5_banks.py
@@ -137,6 +137,21 @@ def png_2_csv(file_name):
         print(idx)
         with open(file_name.replace(".png", f"sub_{str(idx)}.csv"), "wt") as fout:
             fout.write(table_csv)
+    
+    s3_client = boto3.client('s3')
+    
+    try:
+        object_name = file_name.split("/")[-1]
+        bucket = "liberta-leasing-ml"
+        response = s3_client.upload_file(file_name, bucket, object_name)
+        result = object_name
+        
+    except Exception as e:
+        response = None
+        result = "failed transaction"
+        print(str(e))
+        pass
+    return result
             
 def parse(f_path):
     all_files = glob2.glob(os.path.join(f_path, "*/*.png"))
