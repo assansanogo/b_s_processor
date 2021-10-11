2f117f9deb83ee10d8fd2927f554ae1197041d7a
Assan Sanogo <assansanogo@gmail.com>
2021-10-12 00:47:43 +0200

    edit parenthesis

--- lambda_functions/classify_5_banks.py
+++ lambda_functions/classify_5_banks.py
@@ -58,7 +58,7 @@ def classify_liberta_leasing_convert_handler(event, context):
         # when no error :process and returns json
         dest_file = f_path
         dataframe_file = pd.read_excel(dest_file)
-        dataframe_file["Narration_Vectorized"] = dataframe_file["Narration"].apply(lambda x: model_doc2vec.infer_vector(x.split(" "))
+        dataframe_file["Narration_Vectorized"] = dataframe_file["Narration"].apply(lambda x: model_doc2vec.infer_vector(x.split(" ")))
         return {'headers': {'Content-Type':'application/json'}, 
                 'statusCode': 200,
                 'body': json.dumps(str(dataframe_file))}
