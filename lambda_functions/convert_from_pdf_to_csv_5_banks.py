6b2739b0dc5a417fe5d60256d112871ea171e458
Assan Sanogo <assansanogo@gmail.com>
2021-10-11 13:31:35 +0200

    first version of lambda (pdf to png)

---
+++ lambda_functions/convert_from_pdf_to_csv_5_banks.py
@@ -0,0 +1,54 @@
+import PyPDF2
+import tqdm
+from pdf2image import convert_from_path, convert_from_bytes
+import zipFile
+import os
+import glob2
+
+
+def download_url(url):
+    '''
+    utility funcction which downloads pdf to local environment
+    '''
+    # data is going to be read as stream
+    chunk_size=2000
+    r = requests.get(url, stream=True)
+    # the pdf filename is extracted from the presigned url
+    file_name = [el for el in url.split("/") if ".pdf" in el][0]
+    # open a file to dump the stream in
+    with open(f'/tmp/{file_name}', 'wb') as fd:
+        for chunk in r.iter_content(chunk_size):
+            fd.write(chunk)
+    return f'/tmp/{file_name}'
+
+def parse(my_pdf):
+    reader = PyPDF2.PdfFileReader(my_pdf)
+    n_pages = len(reader.pages)
+    for i in tqdm.tqdm(range(n_pages)):
+        writer = PyPDF2.PdfFileWriter()
+        my_page = reader.getPage(i)
+        writer.addPage(my_page)
+        output_filename = my_pdf.replace('.pdf', f'_{str(i)}.pdf')
+        new_dir = '/tmp
+        os.makedirs(new_dir, exist_ok=True)
+        new_path = os.path.join(new_dir, output_filename)
+        with open(new_path, 'wb') as output:
+            writer.write(output)
+        png_path = new_path.replace("pdf","png")
+        img_test = convert_from_path(new_path)[0].save(png_path)
+        
+        os.chdir(new_dir)
+      
+      all_png = glob2.glob(f"{new_dir}/*.png")
+      with ZipFile('my_bank_statement_png.zip','w') as zip:
+          # writing each file one by one for file in png paths:
+          zip.write(png_path)
+      print(os.path. getsize('my_bank_statement_png.zip')
+          
+          
+def convert_from_pdf_2_csv_handler(event, context):
+    input_file_url = event["url"]
+    output_format = event["format"]
+    f_path = download_url(input_file_url)
+    parse(f_path)
+ 
