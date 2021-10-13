b3612b97f7a462d56a370d4191346862eafa33f1
Assan Sanogo <assansanogo@gmail.com>
2021-10-13 16:19:18 +0200

    Update convert_from_pdf_to_png_5_banks.py

--- lambda_functions/step1_convert_to_png/extraction/convert_from_pdf_to_png_5_banks.py
+++ lambda_functions/step1_convert_to_png/extraction/convert_from_pdf_to_png_5_banks.py
@@ -51,8 +51,8 @@ def parse(my_pdf):
 def convert_from_pdf_2_csv_handler(event, context):
     event = json.loads(event["body"])
     
-    #input_file_url = event["url"]
-    #output_format = event["format"]
+    input_file_url = event["url"]
+    output_format = event["format"]
     #f_path = download_url(input_file_url)
     
     
@@ -60,7 +60,7 @@ def convert_from_pdf_2_csv_handler(event, context):
     try:
         # when no error :process and returns json
         #dest_file = parse(f_path)
-        dest_file = str(event.keys())
+        dest_file = str(event["url"])
         return {'headers': {'Content-Type':'application/json'}, 
         'statusCode': 200,
         'body': json.dumps(dest_file)}
