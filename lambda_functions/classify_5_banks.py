a093e888f8ff05a90963c701eec6fb68cac64c4e
Assan Sanogo <assansanogo@gmail.com>
2021-10-19 00:03:23 +0200

    Update classify_5_banks.py

--- lambda_functions/classify_5_banks.py
+++ lambda_functions/classify_5_banks.py
@@ -80,7 +80,7 @@ def classify_liberta_leasing_convert_handler(event, context):
         local_file_name = '/tmp/classified_file.xlsx'
         dataframe_file.to_excel(local_file_name, index=None)
         response = s3_client.upload_file(local_file_name, OUTPUT_BUCKET_NAME, OUTPUT_FILE_NAME)
-        upload_details = s3_client.generate_presigned_url(Bucket=OUTPUT_BUCKET_NAME, Key=OUTPUT_FILE_NAME, ExpiresIn = 100)
+        upload_details = s3_client.generate_presigned_url(Params={"Bucket":OUTPUT_BUCKET_NAME, "Key":OUTPUT_FILE_NAME}, ExpiresIn = 100)
         
         
         return {'headers': {'Content-Type':'application/json'}, 
